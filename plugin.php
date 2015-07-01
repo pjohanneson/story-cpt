1b5c8ed7cb087f2b7fc6a3066b6ae195c25d3b16
Patrick Johanneson <pjohanneson@gmail.com>
2015-07-01 00:06:35 -0500

    Initial commit

---
+++ plugin.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Plugin Name: Fiction Custom Post Type
+Plugin URI: http://patj.ca/wp/plugins/fiction-cpt
+Description: Generates the "Fiction" CPT and the assorted metaboxes & tools to go with it
+Version: 0.1
+Author: Patrick Johanneson
+Author URI: http://patrickjohanneson.com/
+License: GPL v2 or later
+*/
+
+/**
+ * Copyright (c) 2015 . All rights reserved.
+ *
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * http://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * **********************************************************************
+ */
